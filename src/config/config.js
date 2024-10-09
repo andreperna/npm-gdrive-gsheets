1c712185512fc5936380b39bb8e40ffe71c99166
andreperna <andreperna@gmail.com>
2024-10-09 14:17:03 -0300

    build(deps): update package-lock.json and config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 import path from "node:path"
 import dotenv from "dotenv";
-const dotenvFilePath = path.resolve(process.env.HOME , ".dotenv", ".env")
-const keyFilePath = path.resolve(process.env.HOME, ".dotenv", "secret.json")
+// const dotenvFilePath = path.resolve(process.env.HOME , ".dotenv", ".env")
+// const keyFilePath = path.resolve(process.env.HOME, ".dotenv", "secret.json")
 
 const client_email = process.env.GOOGLE_CLIENT_EMAIL
 const private_key = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
 const projectId = process.env.GOOGLE_PROJECT_ID
 const rootFolderId = process.env.ROOT_FOLDER_ID
 
-dotenv.config({path: dotenvFilePath, debug: true})
+// dotenv.config({path: dotenvFilePath, debug: true})
 
 
 
